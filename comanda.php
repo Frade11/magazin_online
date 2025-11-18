410a6e46a149a7d847d2ee4c8e5a1d79f5ab4b31
Frade11 <gurdisartemii@gmail.com>
2025-11-18 19:58:07 +0200

    icon added

--- comanda.php
+++ comanda.php
@@ -69,6 +69,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Plaseaza Comanda - Magazin Online</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/pcWorld-logo.png">
 </head>
 <body>
     <div class="container">
--- vizualizare.php
+++ vizualizare.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vizualizare Comenzi - Magazin Online</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/pcWorld-logo.png">
+
 </head>
 <body>
     <div class="container">
